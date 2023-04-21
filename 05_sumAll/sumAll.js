e79485a2973e99d353a6e18bf22be458674c9714
Zach Barreca <zachbarreca@gmail.com>
2023-04-20 23:11:13 -0400

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return 'ERROR'
+    }
 
+    if (min < 0 || max < 0){
+        return 'ERROR'
+    }
+
+    if (min > max) {
+        let placeholder = min;
+        min = max;
+        max = placeholder;
+    }
+
+    let sum = 0;
+    for (let i = min; i < max+1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
